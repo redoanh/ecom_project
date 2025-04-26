b5a5e7f1d75fccccc782b80f3eac4a97a0f770e2
redoan hossain <rejowankhan8923@gmail.com>
2025-04-26 21:25:56 +0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
